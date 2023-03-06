bb58f56e2ad972ab506f315db1d7790c75041d51
diana <progdidi@gmail.com>
2023-03-06 18:04:53 +0100

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
-   
+   const promoBtn = document.querySelectorAll('.promo__btn');
+
+   promoBtn.forEach((btn) => {
+        btn.addEventListener('mouseover', () => {
+            btn.classList.add('black-btn');
+        })
+
+        btn.addEventListener('mouseout', () => {
+            btn.classList.remove('black-btn');
+        })
+
+   })
 });
