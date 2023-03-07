e0ed9fbb0a3dff3f69fb524f655157919d1cbe18
diana <progdidi@gmail.com>
2023-03-07 16:00:36 +0100

    explore section

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,7 @@ $(function(){
    promoBtn.forEach((btn) => {
         btn.addEventListener('mouseover', () => {
             btn.classList.add('black-btn');
-        })
-
-        btn.addEventListener('mouseout', () => {
-            btn.classList.remove('black-btn');
+            btn.previousElementSibling.classList.remove('black-btn');
         })
 
    })
